d61e202babef9748fe015491982c51adc2650e6e
Ricardo Ruiz <ricardojruiz93@gmail.com>
2025-03-28 09:59:39 +0100

    Add files via upload

--- github-star-counter.py
+++ github-star-counter.py
@@ -1,21 +1,39 @@
 import sys
 import requests
 
-username = sys.argv[1]
-max_number_repos = 1000
 
-url = "https://api.github.com/users/" + username + "/repos?per_page=" + str(max_number_repos)
-response = requests.get(url)
-data = response.json()
+def get_data(username, page, total_count):
+	max_number_repos = 100
+	url = "https://api.github.com/users/" + username + "/repos?per_page=" + str(max_number_repos) + "&page=" + str(page)
+	response = requests.get(url)
+	data = response.json()
 
-total_count = 0
-aux = 107
-print("-"*aux)
-print ("| {:<50} | {:<50} |".format('Repository','Total stars'))
-print("-"*aux)
-for repo in data:
-	print ("| {:<50} | {:<50} |".format( repo["name"], str(repo["stargazers_count"]) ))
-	total_count += repo["stargazers_count"]
-print("-"*aux)
+	repo_counter = 0
+	for repo in data:
+		repo_counter += 1
+		print ("| {:<50} | {:<50} |".format( repo["name"], str(repo["stargazers_count"]) ))
+		total_count += repo["stargazers_count"]
+	if repo_counter == 100:
+		page+=1
+		return get_data(username, page, total_count)
+	return total_count
 
-print("\n[+] Total count: "+ str(total_count))
+
+def main():
+	username = sys.argv[1]
+
+	aux = 107
+	print("-"*aux)
+	print ("| {:<50} | {:<50} |".format('Repository','Total stars'))
+	print("-"*aux)
+
+	page = 1
+	total_count = 0
+	total_count = get_data(username, page, total_count)
+	print ("| {:<50} | {:<50} |".format( "", "" ))
+	print ("| [+] Total count: "+ str(total_count))
+	print ("-"*aux)
+
+
+if __name__ == "__main__":
+    main()
